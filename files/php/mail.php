73740e496fde6468a3eb1074deeefe42633ea90d
AlexTr999 <tronenko.aleksandr@gmail.com>
2021-01-21 14:55:48 +0200

    ArgoDev work

---
+++ files/php/mail.php
@@ -0,0 +1,47 @@
+<?php
+ini_set('display_errors', 1);
+error_reporting( E_ALL );
+$to = "office@agrodom-group.ru";
+$phone = $_POST['phone'];
+$name = $_POST['name'];
+$product = $_POST['product'];
+$subject = 'Заявка со страницы ' + $product;
+$page = 'Заявка со страницы ' + $product;
+$message = '
+<html>
+<body>
+<center>	
+<table border=1 cellpadding=6 cellspacing=0 width=90% bordercolor="#DBDBDB">
+ <tr><td colspan=2 align=center bgcolor="#E4E4E4"><b>Заявка</b></td></tr>
+ <tr>
+  <td><b>Тема</b></td>
+  <td>Заявка на продукт '.$product.', от '.$name.', '.$phone.'</td>
+ </tr>
+ <tr>
+  <td><b>Имя покупателя</b></td>
+  <td>'.$name.'</td>
+ </tr>
+ <tr>
+ <td><b>Телефон</b></td>
+ <td>'.$phone.'</td>
+ </tr>
+ <tr>
+ <td><b>Продукт</b></td>
+ <td>'.$product.'</td>
+ </tr>
+</table>
+</center>	
+</body>
+</html>'; 
+$headers  = "Content-type: text/html; charset=utf-8\r\n";
+if ($phone != '' && $name != '') {
+$result = mail($to, $subject, $message, $headers);
+} else {
+    echo 'Ошибка! product: '.$product.' name: '.$name.' phone: '.$phone;
+}
+if ($result) {
+	echo 'Сообщение успешно отправлено!';
+} else {
+	echo 'Сообщение не отправлено!';
+}
+?>
